717d8677972c423b94cb1793fd69712a267aae46
rfong <rachefo@gmail.com>
2020-02-28 12:43:31 -0800

    fix whitespace

--- _plugins/compile_tags.py
+++ _plugins/compile_tags.py
@@ -2,7 +2,7 @@
 
 '''
 This script generates tag pages for all your post tags for a Jekyll site.
-It is invoked as a side effect of a plugin after post_write.
+It is invoked from a plugin after post_write.
 Run it from the project root if testing.
 
 Current convention expected for tag names is r/[-\w\d]+/
@@ -17,15 +17,15 @@ TAG_DIR = 'tag/'
 # Collect all tags from all posts.
 all_tags = []
 for fname in glob.glob(POST_DIR + '*.md'):
-	with open(fname, 'r') as f:
-		for line in f:
-			line = line.strip().replace('[', '').replace(']', '')
-			# Find the line where tags are specified and cut tags.
-			if line.startswith('tags: '):
-				# TODO: To ensure consistency between blog tags and scraped
-				# tag names, we should probably URI encode on both ends.
-				all_tags += [t.strip() for t in line[len("tags: "):].split(',')]
-				break
+  with open(fname, 'r') as f:
+    for line in f:
+      line = line.strip().replace('[', '').replace(']', '')
+      # Find the line where tags are specified and cut tags.
+      if line.startswith('tags: '):
+        # TODO: To ensure consistency between blog tags and scraped
+        # tag names, we should probably URI encode on both ends.
+        all_tags += [t.strip() for t in line[len("tags: "):].split(',')]
+        break
 all_tags = sorted(list(set(all_tags)))
 
 # Remove old tag pages
@@ -44,5 +44,5 @@ tag: {tag}
 robots: noindex
 ---'''
 for tag in all_tags:
-	with open(TAG_DIR + tag + '.md', 'a') as f:
-		f.write(TAG_PAGE_TEMPLATE.format(tag=tag))
+  with open(TAG_DIR + tag + '.md', 'a') as f:
+    f.write(TAG_PAGE_TEMPLATE.format(tag=tag))
